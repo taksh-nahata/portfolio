f5c2b8ee6472736a888833e97b7f3a0d2a8c9f61
Taksh Nahata <taksh.nahata37@gmail.com>
2026-07-10 11:44:02 -0700

    Enhance portfolio animation and layout by updating viewport settings, refining CSS styles, and implementing dynamic name display in JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const myName = "TAKSH NAHATA"
 
-const logoElement
+const logoElement = document.getElementById("logo");
+const words = myName.split(' ');
+
+const totalHTML = words.map((word, wordIndex) => {
+    const wordDelay = wordIndex * 1.1;
+    const letters = word.split('');
+    
+    const letterHTML = letters.map((letter, letterIndex) => {
+        const initialDelay = 0.5;
+        const letterDelay = letterIndex * 0.18;
+        
+        const totalDelay = initialDelay + wordDelay + letterDelay;
+        return `<span class="letter" style="animation-delay: ${totalDelay}s">${letter}</span>`;
+    }).join('');
+
+    return `<div class="word" style="animation-delay: ${wordDelay}s">${letterHTML}</div>`;
+}).join('');
+logoElement.innerHTML = totalHTML;
